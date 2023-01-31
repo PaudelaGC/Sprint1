61ecee01b1d317ad73ada58129ea9dd813c10f70
PaudelaGC <80599344+PaudelaGC@users.noreply.github.com>
2023-01-31 12:25:13 +0100

    Update E1_4.js
    Exercicis finalitzats i revisats per Marta

--- Entrega1.4/E1_4.js
+++ Entrega1.4/E1_4.js
@@ -31,12 +31,15 @@ let salaries = [
 
 const getEmployee = z => {
     return new Promise((resolve, reject) => {
-        if (employees.some((v) => v.id === z)) {
-            const nom = employees.find((x) => x.id === z).name;
-            resolve(nom);
-        } else {
-            reject(new Error(`No hi ha cap empleat amb l'id ${z}`));
-        }
+        if (!Number.isInteger(z)) {
+            reject(new Error(`El parametre no es un id`));
+        } else
+            if (employees.some((v) => v.id === z)) {
+                const nom = employees.find((x) => x.id === z).name;
+                resolve(nom);
+            } else {
+                reject(new Error(`No hi ha cap empleat amb l'id ${z}`));
+            }
     })
 };
 
@@ -51,7 +54,7 @@ const getSalary = y => {
             });
             resolve(empleat(y, salari));
         } else {
-            reject(new Error(`No hi ha cap empleat amb l'id ${y}`));
+            reject(new Error(`No hi ha cap empleat amb el nom ${y}`));
         }
     })
 
@@ -68,6 +71,7 @@ mostrarEmpleatSalari(1);
 mostrarEmpleatSalari(2);
 mostrarEmpleatSalari(3);
 mostrarEmpleatSalari(4);
+mostrarEmpleatSalari("d");
 
 
 //Nivell 1 Exercici 2: Crea una nova funció asíncrona que cridi a una altra que retorni una Promise que efectuï la seva funció resolve() després de 2 segons de la seva invocació.
@@ -95,3 +99,52 @@ const beureCafe = (x) => {
 }
 
 prendreCafe();
+
+//Nivell 2 Exercici 1: Crea una funció que retorni el doble del número que li passa com a paràmetre després de 2 segons.
+//Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir la funció anterior.
+
+async function duplicar(x) {
+    return await new Promise((resolve, reject) => {
+        console.log("El doble de " + x + " es...")
+        if (Number.isInteger(x)) {
+
+            x = x * 2;
+            setTimeout(() => {
+                resolve(x);
+            }, 2000);
+        } else {
+            reject(new Error(`El parametre no es un número`));
+        }
+    })
+
+
+
+}
+async function sumarTresDobles(x, y, z) {
+    let dx = 0, dy = 0, dz = 0;
+    try {
+        dx = await duplicar(x);
+        console.log(dx);
+        dy = await duplicar(y);
+        console.log(dy);
+        dz = await duplicar(z);
+        console.log(dz);
+        let total = dx + dy + dz;
+        console.log("La suma dels tres dobles es " + total);
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+sumarTresDobles(2, "e", 4);
+sumarTresDobles(5, 10, 20);
+sumarTresDobles(5, 10, 333333);
+sumarTresDobles(2, 4);
+sumarTresDobles(1, 2, 3, 4);
+
+
+//Nivell 3 Exercici 1: Força i captura tants errors com puguis dels nivells 1 i 2.
+//Exercici resolt en els rejects dels exercicis anteriors
+
+
+//Corretgit per Marta
