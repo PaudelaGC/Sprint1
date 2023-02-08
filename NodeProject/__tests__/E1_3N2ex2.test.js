c123bdd7629728c1ae7b376b1c945a8202e3bddb
PaudelaGC <80599344+PaudelaGC@users.noreply.github.com>
2023-02-08 12:51:29 +0100

    Update E1_3N2ex2.test.js

--- NodeProject/__tests__/E1_3N2ex2.test.js
+++ NodeProject/__tests__/E1_3N2ex2.test.js
@@ -1,4 +1,4 @@
-const trobarSalari = require('./E1_3N2ex2');
+const trobarSalari = require('./app/E1_3N2ex2');
 
 test("Pasar una ID que existeixi et retorna el salari de l'empleat amb aquesta ID", () => {
     expect(trobarSalari(1)).toBe(4000);
