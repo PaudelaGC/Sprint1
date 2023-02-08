832623dfdaabe268bd55ab84e43dadf517865224
PaudelaGC <80599344+PaudelaGC@users.noreply.github.com>
2023-02-08 12:51:09 +0100

    Add files via upload

---
+++ NodeProject/__tests__/E1_3N2ex2.test.js
@@ -0,0 +1,5 @@
+const trobarSalari = require('./E1_3N2ex2');
+
+test("Pasar una ID que existeixi et retorna el salari de l'empleat amb aquesta ID", () => {
+    expect(trobarSalari(1)).toBe(4000);
+})
