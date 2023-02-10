fd41e8c80bf45ac0e52487fc39382b9a85e311be
PaudelaGC <80599344+PaudelaGC@users.noreply.github.com>
2023-02-10 22:09:28 +0100

    Update E1_4N1ex2.test.js

--- NodeProject/__tests__/E1_4N1ex2.test.js
+++ NodeProject/__tests__/E1_4N1ex2.test.js
@@ -2,6 +2,8 @@ const beureCafe = require('./E1_4N1ex2');
 jest.useFakeTimers();
 jest.spyOn(global, 'setTimeout');
 
+
+describe('suite prendre café', () => {
 test("cridar la funció funciona de forma normal en el temps establert", () => {
     beureCafe();
     expect(setTimeout).toHaveBeenCalledTimes(1);
@@ -15,4 +17,5 @@ test("cridar la funció amb cap paràmetre la invoca de forma normal", async() =
     beureCafe(9);
     expect(setTimeout).toHaveBeenCalledTimes(3);
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 2000);
+})
 })
